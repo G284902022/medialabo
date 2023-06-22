26cc766c1a9ee01cd108962e1fff5c084235b977
G284902022 <228490@st.takushoku-u.ac.jp>
2023-06-22 16:54:12 +0900

    new

--- kazuate.js
+++ kazuate.js
@@ -17,7 +17,7 @@ function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
     let yoso = 4;
     console.log(kaisu + '回目の予想:' + yoso); 
-    if(kaisu > 4) {
+    if(kaisu > 3) {
     console.log('答えは '+ kotae + ' でした．すでにゲームは終わっています');
     }
     else if(kaisu === 3 && kotae !== yoso) {
